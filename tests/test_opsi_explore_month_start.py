60a64c954ac4bc55b0c50d10a03cc5e0f025e4ef
desktop <jnliuyudai@163.com>
2026-09-01 03:13:37 +0800

    fix(opsi): defer daily during monthly exploration

--- tests/test_opsi_explore_month_start.py
+++ tests/test_opsi_explore_month_start.py
@@ -1,5 +1,6 @@
 import unittest
 
+from module.campaign.os_run import OSCampaignRun
 from module.os.tasks.explore import OpsiExplore
 
 
@@ -40,6 +41,42 @@ class OpsiExploreMonthStartTest(unittest.TestCase):
         explore._disable_obscure_at_month_start()
 
         self.assertEqual(explore.config.set_calls, [])
+
+
+class OpsiDailyExploreGuardTest(unittest.TestCase):
+    class Config:
+        def __init__(self):
+            self.delay_calls = []
+            self.stop_calls = 0
+
+        def task_delay(self, **kwargs):
+            self.delay_calls.append(kwargs)
+
+        def task_stop(self):
+            self.stop_calls += 1
+
+    class Runner:
+        opsi_daily = OSCampaignRun.opsi_daily
+
+        def __init__(self):
+            self.config = OpsiDailyExploreGuardTest.Config()
+            self.load_calls = 0
+
+        def is_in_opsi_explore(self):
+            return True
+
+        def load_campaign(self):
+            self.load_calls += 1
+            raise AssertionError('OpsiDaily entered Operation Siren during OpsiExplore')
+
+    def test_daily_is_delayed_without_entering_opsi_while_explore_is_running(self):
+        runner = self.Runner()
+
+        runner.opsi_daily()
+
+        self.assertEqual(runner.load_calls, 0)
+        self.assertEqual(runner.config.delay_calls, [{'server_update': True}])
+        self.assertEqual(runner.config.stop_calls, 1)
 
 
 if __name__ == '__main__':
