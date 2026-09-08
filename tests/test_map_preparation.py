5ca56dec0ac694a0226e82cd62d46a5ab2a6571f
desktop <jnliuyudai@163.com>
2026-09-08 17:13:36 +0800

    fix(map): prefer upstream templates over local matching fallback

--- tests/test_map_preparation.py
+++ tests/test_map_preparation.py
@@ -39,12 +39,10 @@ class MapPreparationTest(unittest.TestCase):
         operation.map_clear_percentage_timer = MapOperation.map_clear_percentage_timer
         return operation
 
-    def test_cn_2_4_dialog_uses_color_fallback_when_default_match_misses(self):
+    def test_cn_2_4_dialog_matches_updated_upstream_template(self):
         operation = self.operation('2-4-dialog.png')
 
-        self.assertFalse(operation.appear(MAP_PREPARATION, offset=(20, 20)))
-        self.assertTrue(operation.match_template_color(
-            MAP_PREPARATION, offset=(20, 20), similarity=0.8))
+        self.assertTrue(operation.appear(MAP_PREPARATION, offset=(20, 20)))
         self.assertIs(operation.handle_map_preparation(), MAP_PREPARATION)
 
     def test_other_pages_are_rejected(self):
@@ -58,21 +56,6 @@ class MapPreparationTest(unittest.TestCase):
 
         hard = self.operation('./assets/cn/map/MAP_PREPARATION_HARD.png')
         self.assertIs(hard.handle_map_preparation(), MAP_PREPARATION_HARD)
-
-    def test_color_mismatch_rejects_same_luma_pattern(self):
-        operation = self.operation('2-4-dialog.png')
-        image = operation.device.image.copy()
-        image[497:525, 975:1094] = image[497:525, 975:1094][:, :, ::-1]
-        operation.device.image = image
-        self.assertTrue(MAP_PREPARATION.match_luma(image, offset=(20, 20), similarity=0.8))
-        self.assertFalse(operation.match_template_color(
-            MAP_PREPARATION, offset=(20, 20), similarity=0.8))
-        self.assertIsNone(operation.handle_map_preparation())
-
-    def test_color_fallback_is_cn_only(self):
-        operation = self.operation('2-4-dialog.png', server_name='en')
-        server.server = 'en'
-        self.assertIsNone(operation.handle_map_preparation())
 
 
 if __name__ == '__main__':
